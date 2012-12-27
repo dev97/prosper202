be19c20760ee972cdaa1e48df5508fa378e3da25
Yuri Karaban <dev@dev97.com>
2012-12-27 16:35:08 +0200

    fix timezone for PHP 5.4

--- 202-config/functions-auth.php
+++ 202-config/functions-auth.php
@@ -89,30 +89,30 @@ class AUTH {
 			$user_timezone = $_SESSION['user_timezone'];	
 		}
 		
-		if ($user_timezone == '-12') { @putenv('TZ=NZS-12NZD'); } 
-		if ($user_timezone == '-11') { @putenv('TZ=SST11'); }    
-		if ($user_timezone == '-10') { @putenv('TZ=HST10HDT'); }    
-		if ($user_timezone == '-9') { @putenv('TZ=AKS9AKD'); }    
-		if ($user_timezone == '-8') { @putenv('TZ=PST8PDT'); }    
-		if ($user_timezone == '-7') { @putenv('TZ=MST7MDT'); }    
-		if ($user_timezone == '-6') { @putenv('TZ=CST6CDT'); }    
-		if ($user_timezone == '-5') { @putenv('TZ=EST5EDT'); }      
-		if ($user_timezone == '-4') { @putenv('TZ=AST4ADT'); }    
-		if ($user_timezone == '-3.5') { @putenv('TZ=NST3:30NDT'); }    
-		if ($user_timezone == '-3') { @putenv('TZ=BST3'); }    
-		if ($user_timezone == '-2') { @putenv('TZ=FST2FDT'); }    
-		if ($user_timezone == '0') { @putenv('TZ=Europe/London'); }    
-		if ($user_timezone == '1') { @putenv('TZ=Europe/Paris'); }    
-		if ($user_timezone == '2') { @putenv('TZ=Asia/Istanbul'); }    
-		if ($user_timezone == '3') { @putenv('TZ=Asia/Kuwait'); }     
-		if ($user_timezone == '3.5') { @putenv('TZ=Asia/Tehran'); }    
-		if ($user_timezone == '5.5') { @putenv('TZ=IST-5:30'); }    
-		if ($user_timezone == '7') { @putenv('TZ=Asia/Bangkok'); }    
-		if ($user_timezone == '8') { @putenv('TZ=Asia/Hong_Kong'); }    
-		if ($user_timezone == '9') { @putenv('TZ=Asia/Tokyo'); }    
-		if ($user_timezone == '9.5') { @putenv('TZ=Australia/Darwin'); }    
-		if ($user_timezone == '10') { @putenv('TZ=Australia/Sydney'); }    
-		if ($user_timezone == '12') {  @putenv('TZ=Pacific/Auckland'); }     
+		if ($user_timezone == '-12') { date_default_timezone_set('NZS-12NZD'); } 
+		if ($user_timezone == '-11') { date_default_timezone_set('SST11'); }    
+		if ($user_timezone == '-10') { date_default_timezone_set('HST10HDT'); }    
+		if ($user_timezone == '-9') { date_default_timezone_set('AKS9AKD'); }    
+		if ($user_timezone == '-8') { date_default_timezone_set('PST8PDT'); }    
+		if ($user_timezone == '-7') { date_default_timezone_set('MST7MDT'); }    
+		if ($user_timezone == '-6') { date_default_timezone_set('CST6CDT'); }    
+		if ($user_timezone == '-5') { date_default_timezone_set('EST5EDT'); }      
+		if ($user_timezone == '-4') { date_default_timezone_set('AST4ADT'); }    
+		if ($user_timezone == '-3.5') { date_default_timezone_set('NST3:30NDT'); }    
+		if ($user_timezone == '-3') { date_default_timezone_set('BST3'); }    
+		if ($user_timezone == '-2') { date_default_timezone_set('FST2FDT'); }    
+		if ($user_timezone == '0') { date_default_timezone_set('Europe/London'); }    
+		if ($user_timezone == '1') { date_default_timezone_set('Europe/Paris'); }    
+		if ($user_timezone == '2') { date_default_timezone_set('Asia/Istanbul'); }    
+		if ($user_timezone == '3') { date_default_timezone_set('Asia/Kuwait'); }     
+		if ($user_timezone == '3.5') { date_default_timezone_set('Asia/Tehran'); }    
+		if ($user_timezone == '5.5') { date_default_timezone_set('IST-5:30'); }    
+		if ($user_timezone == '7') { date_default_timezone_set('Asia/Bangkok'); }    
+		if ($user_timezone == '8') { date_default_timezone_set('Asia/Hong_Kong'); }    
+		if ($user_timezone == '9') { date_default_timezone_set('Asia/Tokyo'); }    
+		if ($user_timezone == '9.5') { date_default_timezone_set('Australia/Darwin'); }    
+		if ($user_timezone == '10') { date_default_timezone_set('Australia/Sydney'); }    
+		if ($user_timezone == '12') {  date_default_timezone_set('Pacific/Auckland'); }     
 
 	}
 	
