9f676bae4969dbc87bb3335e86c1ec68d190dceb
Yuri Karaban <dev@dev97.com>
2013-04-06 12:44:39 +0300

    set c-basic-offset to 4

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ((nil . ((require-final-newline . nil)
          (indent-tabs-mode . t)
          (backward-delete-char-untabify-method . nil)))
- (c-mode . ((c-basic-offset . 8))))
+ (c-mode . ((c-basic-offset . 4))))
