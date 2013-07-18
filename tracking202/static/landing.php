43d130243091400f0ecbab90473e5563e7ab214f
Yuri Karaban <dev@dev97.com>
2013-07-18 17:23:00 +0300

    urlencode cookies

--- tracking202/static/landing.php
+++ tracking202/static/landing.php
@@ -113,17 +113,19 @@ function createCookie(name,value,days) {
 		var expires = "; expires="+date.toGMTString();
 	}
 	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
+	document.cookie = encodeURIComponent(name)+"="+
+		encodeURIComponent(value)+expires+"; path=/";
 
 }
 
 function readCookie(name) {
-	var nameEQ = name + "=";
+	var nameEQ = encodeURIComponent(name) + "=";
 	var ca = document.cookie.split(';');
 	for(var i=0;i < ca.length;i++) {
 		var c = ca[i];
 		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		if (c.indexOf(nameEQ) == 0)
+			return decodeURIComponent(c.substring(nameEQ.length,c.length));
 	}
 	return null;
 
