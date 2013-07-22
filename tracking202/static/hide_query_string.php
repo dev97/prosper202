0107316534cac0724e96a038ec22849986a95c94
Yuri Karaban <dev@dev97.com>
2013-07-22 20:18:41 +0300

    POST keyword, c1, c2, c3, c4 parameters to landing page
    (when hide_query_string enabled)

--- tracking202/static/hide_query_string.php
+++ tracking202/static/hide_query_string.php
@@ -8,13 +8,26 @@ if (isset($tracker_row['hide_query_string']) &&
 
   if (p !== -1) {
     createCookie('tracking202ignore', '1');
-    createCookie('tracking202keyword', <?= json_encode($keyword) ?>);
-    createCookie('tracking202c1', <?= json_encode($c1) ?>);
-    createCookie('tracking202c2', <?= json_encode($c2) ?>);
-    createCookie('tracking202c3', <?= json_encode($c3) ?>);
-    createCookie('tracking202c4', <?= json_encode($c4) ?>);
 
-    document.location = l.substr(0, p);
+    var e, v, form = document.createElement('form');
+    form.style.display = 'none';
+<?php
+	foreach (array('keyword', 'c1', 'c2', 'c3', 'c4') as $name)  {
+		$cookie_name = "tracking202$name";
+		$value = json_encode($GLOBALS[$name]);
+		echo <<<EOF
+    e = document.createElement('input');
+    v = $value; e.name = "$name"; e.value = v;
+    form.appendChild(e);
+    createCookie("$cookie_name", v);
+
+EOF;
+    }
+?>
+    document.body.appendChild(form);
+    form.method = 'POST';
+    form.action = l.substr(0, p);
+    form.submit();
   }
 }());
 <?php
