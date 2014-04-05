417346340f2f1894b47a625be2777a6a66aa6d38
Yuri Karaban <dev@dev97.com>
2014-04-05 11:29:41 +0300

    save t202id in cookies and use $_GET directly for getting parameters

--- tracking202/static/hide_query_string.php
+++ tracking202/static/hide_query_string.php
@@ -12,9 +12,9 @@ if (isset($tracker_row['hide_query_string']) &&
     var e, v, form = document.createElement('form');
     form.style.display = 'none';
 <?php
-	foreach (array('keyword', 'c1', 'c2', 'c3', 'c4') as $name)  {
+	foreach (array('t202id', 'keyword', 'c1', 'c2', 'c3', 'c4') as $name)  {
 		$cookie_name = "tracking202$name";
-		$value = json_encode($GLOBALS[$name]);
+		$value = json_encode(isset($_GET[$name]) ? $_GET[$name] : '');
 		echo <<<EOF
     e = document.createElement('input');
     v = $value; e.name = "$name"; e.value = v;
